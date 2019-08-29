c915b9750087937830d7653fd07bfb41352cfdb7
zhangdongkun <zhangdongkun@gmail.com>
2019-08-29 23:05:06 +0800

    first commit

---
+++ src/cn/zdk/thread01/TreadTest1.java
@@ -0,0 +1,36 @@
+package cn.zdk.thread01;
+
+/**
+ * @description 手动创建一个线程练练手
+ * @author: zhangDongkun
+ * @date: 2019-08-29 22:53
+ **/
+
+public class TreadTest1 {
+
+
+    public static void main(String[] args) {
+
+        Thread t1 = new Thread(new Thread01());
+        Thread t2 = new Thread(new Thread02());
+        t1.start();
+        t2.start();
+
+    }
+}
+
+class Thread01 implements Runnable {
+
+    @Override
+    public void run() {
+        System.err.println("当前线程：｛｝" + Thread.currentThread().getId());
+    }
+}
+
+class Thread02 implements Runnable {
+    @Override
+    public void run() {
+        System.err.println("当前线程：｛｝" + Thread.currentThread().getId());
+
+    }
+}
